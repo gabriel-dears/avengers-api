865aba8e460358811e7b72eb52813600a378f928
Gabriel Soares <gabrielalcantarars@gmail.com>
2024-03-28 23:55:31 -0300

    avengers api basics

---
+++ src/test/kotlin/org/app/avengers/AvengersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.app.avengers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvengersApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
