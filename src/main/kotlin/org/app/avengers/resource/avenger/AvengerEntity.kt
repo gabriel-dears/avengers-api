650d364445e878e347a8db6119d45181ff0ca511
Gabriel Soares <gabrielalcantarars@gmail.com>
2024-03-30 15:08:50 -0300

    Fixing Entity database mapping name

--- src/main/kotlin/org/app/avengers/resource/avenger/AvengerEntity.kt
+++ src/main/kotlin/org/app/avengers/resource/avenger/AvengerEntity.kt
@@ -5,10 +5,12 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 import org.app.avengers.domain.avenger.Avenger
 import org.jetbrains.annotations.NotNull
 
-@Entity
+@Entity(name = "avenger")
+//@Table(name = "avenger")
 data class AvengerEntity(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
