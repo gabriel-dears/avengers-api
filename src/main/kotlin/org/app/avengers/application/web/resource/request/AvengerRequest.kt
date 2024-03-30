73f38b8f86f250c9c85d9688a35f2272c3b264d9
Gabriel Soares <gabrielalcantarars@gmail.com>
2024-03-29 23:15:55 -0300

    adding files to AvengerResource

---
+++ src/main/kotlin/org/app/avengers/application/web/resource/request/AvengerRequest.kt
@@ -0,0 +1,31 @@
+package org.app.avengers.application.web.resource.request
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotEmpty
+import org.app.avengers.domain.avenger.Avenger
+import org.jetbrains.annotations.NotNull
+
+data class AvengerRequest(
+        @field:NotNull
+        @field:NotBlank
+        @field:NotEmpty
+        val nick: String,
+        @field:NotNull
+        @field:NotBlank
+        @field:NotEmpty
+        val person: String,
+        val description: String,
+        val history: String
+) {
+    fun toAvenger() = Avenger(nick = nick, person = person, description = description, history = history)
+
+    companion object {
+        fun to(id: Long?, request: AvengerRequest) = Avenger(
+                nick = request.nick,
+                person = request.person,
+                description = request.description,
+                history = request.history
+        )
+    }
+
+}
---
+++ src/main/kotlin/org/app/avengers/application/web/resource/response/AvengerResponse.kt
@@ -0,0 +1,24 @@
+package org.app.avengers.application.web.resource.response
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotEmpty
+import org.app.avengers.domain.avenger.Avenger
+import org.jetbrains.annotations.NotNull
+
+data class AvengerResponse(
+        val id: Long,
+        val nick: String,
+        val person: String,
+        val description: String?,
+        val history: String?
+) {
+        companion object {
+                fun from(avenger: Avenger) = AvengerResponse(
+                        id = avenger.id,
+                        nick = avenger.nick,
+                        person = avenger.person,
+                        description = avenger.description,
+                        history = avenger.history
+                )
+        }
+}
